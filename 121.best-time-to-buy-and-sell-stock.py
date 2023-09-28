908b2dd188555bb735a5305749e1ff5c88c0f9ae
akmhmgc <akimuhamaguchi@icloud.com>
2023-09-29 08:43:24 +0900

    121

---
+++ 121.best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=121 lang=python3
+#
+# [121] Best Time to Buy and Sell Stock
+#
+
+# @lc code=start
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left = 0 #Buy
+        right = 1 #Sell
+        max_profit = 0
+        while right < len(prices):
+            currentProfit = prices[right] - prices[left] #our current Profit
+            if prices[left] < prices[right]:
+                max_profit =max(currentProfit,max_profit)
+            else:
+                left = right
+            right += 1
+        return max_profit
+
+# @lc code=end
+
