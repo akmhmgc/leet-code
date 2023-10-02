bdb0f818998d3e6db0d29e671807ff587885a604
akmhmgc <akimuhamaguchi@icloud.com>
2023-10-03 07:05:04 +0900

    45

--- 45.jump-game-ii.py
+++ 45.jump-game-ii.py
@@ -10,18 +10,17 @@ class Solution:
         ans = 0
         end = 0
         farthest = 0
-
-        # Implicit BFS
-        for i in range(len(nums) - 1):
-            farthest = max(farthest, i + nums[i])
-            if farthest >= len(nums) - 1:
+        n = len(nums)
+        for i in range(n - 1):
+            farthest = max(farthest, nums[i] + i)
+            if farthest >= n - 1:
                 ans += 1
                 break
-            if i == end:      # Visited all the items on the current level
-                ans += 1        # Increment the level
-                end = farthest  # Make the queue size for the next level
-
+            
+            if i == end:
+                ans += 1
+                end = farthest
         return ans
-        
+            
 # @lc code=end
 
