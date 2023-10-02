e656177b4db8f4635c38ef32e82d9acb894aac99
akmhmgc <akimuhamaguchi@icloud.com>
2023-10-02 21:05:32 +0900

    45

--- 45.jump-game-ii.py
+++ 45.jump-game-ii.py
@@ -7,17 +7,21 @@
 # @lc code=start
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        n = len(nums)
-        min_steps = [10**10] * n
-        min_steps[0] = 0
-        for i in range(n):
-            current_step = min_steps[i]
-            if current_step == 10 ** 10 : continue
+        ans = 0
+        end = 0
+        farthest = 0
 
-            for j in range(i, min(nums[i] + i + 1, n)):
-                min_steps[j] = min(min_steps[j], current_step + 1)
-        return min_steps[-1]
-            
+        # Implicit BFS
+        for i in range(len(nums) - 1):
+            farthest = max(farthest, i + nums[i])
+            if farthest >= len(nums) - 1:
+                ans += 1
+                break
+            if i == end:      # Visited all the items on the current level
+                ans += 1        # Increment the level
+                end = farthest  # Make the queue size for the next level
+
+        return ans
         
 # @lc code=end
 
