4d7a6f64bd8bb829a8b0334511d712dbb50cab5d
akmhmgc <akimuhamaguchi@icloud.com>
2023-10-04 09:11:15 +0900

    274

---
+++ 274.h-index.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=274 lang=python3
+#
+# [274] H-Index
+#
+
+# @lc code=start
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        sorted_citatios = sorted(citations)
+        n = len(citations)
+        for i in range(n):
+            index = len(sorted_citatios) - i
+            if sorted_citatios[i] >= index:
+                return index
+        return 0
+        
+# @lc code=end
+
