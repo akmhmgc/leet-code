390947d881a67c22cfeed852674a34174dd8c758
akmhmgc <akimuhamaguchi@icloud.com>
2023-10-06 07:25:25 +0900

    380

--- 380.insert-delete-get-random-o-1.py
+++ 380.insert-delete-get-random-o-1.py
@@ -10,24 +10,37 @@ import random
 class RandomizedSet:
 
     def __init__(self):
-        self.s = set()
+        self.value_map = {}
+        self.value_list = []
 
     def insert(self, val: int) -> bool:
-        if val in self.s:
+        if val in self.value_map:
             return False
         else:
-            self.s.add(val)
+            self.value_list.append(val)
+            self.value_map[val] = len(self.value_list) - 1
             return True
 
     def remove(self, val: int) -> bool:
-        if val in self.s:
-            self.s.remove(val)
-            return True
-        else:
+        if val not in self.value_map:
             return False
+        else:
+            list_last_value = self.value_list[-1]
+
+            removed_value = val
+            removed_value_index = self.value_map[removed_value]
+
+            self.value_list[-1] = removed_value
+            self.value_list[removed_value_index] = list_last_value
+            self.value_list.pop()
+
+            self.value_map[list_last_value] = removed_value_index
+            self.value_map.pop(removed_value)
+
+            return True
 
     def getRandom(self) -> int:
-        return random.choice(tuple(self.s))
+        return random.choice(self.value_list)
     
 
 # Your RandomizedSet object will be instantiated and called as such:
