c20fd749dd96959e7bee478121919f161fa68ef1
akmhmgc <akimuhamaguchi@icloud.com>
2023-10-13 14:25:01 +0900

    12

---
+++ 12.integer-to-roman.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=12 lang=python3
+#
+# [12] Integer to Roman
+#
+
+# @lc code=start
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        # creating dictionary
+        num_maps = {
+            1000: "M",
+            900: "CM",
+            500: "D",
+            400: "CD",
+            100: "C",
+            90: "XC",
+            50: "L",
+            40: "XL",
+            10: "X",
+            9: "IX",
+            5: "V",
+            4: "IV",
+            1: "I",
+        }
+        ans = ""
+        for i in sorted(num_maps.keys(), reverse=True):
+            mod, num = divmod(num, i)
+            ans += num_maps[i] * mod
+        return ans
+        
+# @lc code=end
+
