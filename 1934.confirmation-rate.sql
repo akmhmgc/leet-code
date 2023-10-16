82cb17cee7c828c0a44d6be32c37e5fd0e5f2105
akmhmgc <akimuhamaguchi@icloud.com>
2023-10-17 07:46:39 +0900

    1934

--- 1934.confirmation-rate.sql
+++ 1934.confirmation-rate.sql
@@ -12,9 +12,8 @@ select
   round(
     sum(
       case
-        when C.action = 'timeout' then 0
-        when C.action is null then 0
-        else 1
+        when C.action = 'confirmed' then 1
+        else 0
       end
     ) / count(*)
   , 2) confirmation_rate
