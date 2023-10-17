b1b8efb3e5c1b316517e36059d86abbf40c99b7b
akmhmgc <akimuhamaguchi@icloud.com>
2023-10-17 09:59:23 +0900

    6

--- 6.zigzag-conversion.py
+++ 6.zigzag-conversion.py
@@ -7,25 +7,16 @@
 # @lc code=start
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        list = [''] * numRows
-        n = len(s)
-        print(n)
-        i = 0
-        while True:
-            if i > n - 1: break
-            for j in range(0, numRows):
-                print(f'{i}: {list}')
-                list[j] += s[i]
-                i += 1
-                if i > n - 1: break
+        if numRows == 1 or numRows >= len(s): return s
 
-            for j in range(numRows - 2, 0, -1):
-                print(f'{i}: {list}')
-                list[j] += s[i]
-                i += 1
-                if i > n - 1: break
+        list = [''] * numRows
+        index = 0
+        for st in s:
+            if index == 0: step = 1
+            if index == numRows - 1: step = -1
+            list[index] += st
+            index += step
         return ''.join(list)
-            
 
 # @lc code=end
 
