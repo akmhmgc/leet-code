45f7e7456f202a444f924e5d9abbf3ad325cec25
akmhmgc <akimuhamaguchi@icloud.com>
2023-10-19 07:31:48 +0900

    68

--- 68.text-justification.py
+++ 68.text-justification.py
@@ -7,39 +7,18 @@
 # @lc code=start
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        word_list = []
-        index = -1
-        count = 0
-        for word in words:
-            if count < len(word) + 1:
-                word_list.append([word])
-                index += 1
-                count = maxWidth - len(word)
-            else:
-                word_list[index].append(word)
-                count -= len(word) + 1
+        res, line, width = [], [], 0
+
+        for w in words:
+            if width + len(w) + len(line) > maxWidth:
+                for i in range(maxWidth - width):
+                    # 残りのスペースを開いているスペースに左から順に入れていくe
+                    line[i % (len(line) - 1 or 1)] += ' '
+                res, line, width = res + [''.join(line)], [], 0
+            line += [w]
+            width += len(w)
         
-        ans = []
-        for i in range(0, len(word_list) - 1):
-            word_count = len(word_list[i])
-            word_lengh = 0
-            for w in word_list[i]:
-                word_lengh += len(w)
-            
-            if word_count == 1:
-                ans.append(word_list[i][0] + " " * (maxWidth - word_lengh))
-                continue
-            
-            rest_space = maxWidth - word_lengh
-            space_count, mod = divmod(rest_space, word_count - 1)
-            for j in range(mod):
-                word_list[i][j] += ' '
-            ans.append((' ' * space_count).join(word_list[i]))
-        # 最後
-        last = ' '.join(word_list[-1])
-        last += ' ' * (maxWidth - len(last))
-        ans.append(last)
-        return ans
+        return res + [' '.join(line).ljust(maxWidth)]
 
 # @lc code=end
 
