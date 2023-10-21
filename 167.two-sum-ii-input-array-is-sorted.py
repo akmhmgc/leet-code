c95c32731bd375d3772587eb247d2708c5fd8a05
akmhmgc <akimuhamaguchi@icloud.com>
2023-10-21 09:30:21 +0900

    167

---
+++ 167.two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=167 lang=python3
+#
+# [167] Two Sum II - Input Array Is Sorted
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        remains = {}
+        for i, num in enumerate(numbers):
+            i += 1
+            remain = target - num
+            if num in remains:
+                return [remains[num], i]
+            else:
+                remains[remain] = i
+# @lc code=end
+
