bdb6f3697c1b2dc8f969d631b170e3caf7ee6e23
akmhmgc <akimuhamaguchi@icloud.com>
2023-10-26 07:20:32 +0900

    2356

---
+++ 2356.number-of-unique-subjects-taught-by-each-teacher.sql
@@ -0,0 +1,15 @@
+--
+-- @lc app=leetcode id=2356 lang=mysql
+--
+-- [2356] Number of Unique Subjects Taught by Each Teacher
+--
+
+-- @lc code=start
+# Write your MySQL query statement belowse
+select
+  teacher_id,
+  count(distinct(subject_id)) cnt
+  from Teacher
+  group by teacher_id
+-- @lc code=end
+
