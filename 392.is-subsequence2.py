3af817929c43bae65e0dda9f764f29095e34f745
akmhmgc <akimuhamaguchi@icloud.com>
2023-10-27 07:16:12 +0900

    392

--- 392.is-subsequence2.py
+++ 392.is-subsequence2.py
@@ -7,10 +7,9 @@
 # @lc code=start
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        i, j = 0, 0
+        i,j= 0 ,0
         while i < len(s) and j < len(t):
-            if s[i] == t[j]:
-                i += 1
+            if s[i] == t[j]: i += 1
             j += 1
         return i == len(s)
 # @lc code=end
