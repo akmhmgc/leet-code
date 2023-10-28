cabbe2feb98eb9b75b6979a07b01ddf66a86da2b
akmhmgc <akimuhamaguchi@icloud.com>
2023-10-28 09:10:51 +0900

    30

---
+++ 30.substring-with-concatenation-of-all-words.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=30 lang=python3
+#
+# [30] Substring with Concatenation of All Words
+#
+
+# @lc code=start
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:  
+        hmap = Counter(words)
+        wlen =len(words[0]) 
+        tlen = len(words)*wlen
+
+        # 長さtlenの文字列がwordsリストの文字列を組み合わせたものかどうかをチェック
+        def checksubstr(substr,hmap):
+            # every word is of same len, so we skip that amount 
+            for i in range(0,len(substr),wlen):
+                if(hmap[substr[i:i+wlen]] != 0 ):
+                    hmap[substr[i:i+wlen]] -= 1
+                else:
+                    return False
+            return True 
+        res = []
+        start = 0
+        for i in range(tlen,len(s)+1):
+            if( checksubstr(s[start:i] , copy.deepcopy(hmap) ) ):
+                res.append(start)
+            start += 1
+
+        return res
+# @lc code=end
+
