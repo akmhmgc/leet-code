7ee8edeba9617ac86915ea967991e82e63f3bce4
akmhmgc <akimuhamaguchi@icloud.com>
2023-10-31 14:28:49 +0900

    610

---
+++ 610.triangle-judgement.sql
@@ -0,0 +1,35 @@
+--
+-- @lc app=leetcode id=610 lang=mysql
+--
+-- [610] Triangle Judgement
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+
+select
+  x,y,z,
+  case
+    when x >= y and x >= z then (
+      case
+        when  x < y + z then 'Yes'
+        else 'No'
+      end
+    )
+    when y >= x and y >= z then (
+      case
+        when  y < x + z then 'Yes'
+        else 'No'
+      end
+    )
+    when z >= x and z >= y then (
+      case
+        when  z < x + y then 'Yes'
+        else 'No'
+      end
+    )
+  end triangle
+  from Triangle
+-- @lc code=end
+
