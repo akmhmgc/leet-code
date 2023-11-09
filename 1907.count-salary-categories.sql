1c18eb27e543f3b4407353aa800836d80c570990
akmhmgc <akimuhamaguchi@icloud.com>
2023-11-09 23:15:02 +0900

    1907

---
+++ 1907.count-salary-categories.sql
@@ -0,0 +1,15 @@
+--
+-- @lc app=leetcode id=1907 lang=mysql
+--
+-- [1907] Count Salary Categories
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+select
+  'Low Salary' category
+  count(*) accounts_count
+  from Accounts
+  
+-- @lc code=end
+
