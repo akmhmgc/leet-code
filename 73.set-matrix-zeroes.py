c5f9aa533541dd009072b7248a7695bf4a18a721
akmhmgc <akimuhamaguchi@icloud.com>
2023-11-12 08:52:05 +0900

    73

--- 73.set-matrix-zeroes.py
+++ 73.set-matrix-zeroes.py
@@ -10,15 +10,15 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        empty_row_indexes, empty_column_indexes= [], []
+        empty_row_indexes, empty_column_indexes= set(), set()
         rows,columns = len(matrix), len(matrix[0])
         
         for i in range(rows):
             for j in range(columns):
                 if matrix[i][j] != 0: continue
                 
-                empty_row_indexes.append(i)
-                empty_column_indexes.append(j)
+                empty_row_indexes.add(i)
+                empty_column_indexes.add(j)
         
         for i in empty_row_indexes:
             for j in range(columns):
