ee458829ffa12f5dfa84d4b6b7fa4b3e6a0c742f
akmhmgc <akimuhamaguchi@icloud.com>
2024-06-05 21:11:01 +0900

    206

--- 206.reverse-linked-list2.py
+++ 206.reverse-linked-list2.py
@@ -12,14 +12,12 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        return self.reverse(head, None)
-
-    def reverse(self, cur, prev):
-        if cur is None:
-            return prev
-        else:
-            next = cur.next
-            cur.next = prev
-        return self.reverse(next, cur)
+        prev, curr = None, head
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
 # @lc code=end
 
