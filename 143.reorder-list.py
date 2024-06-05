d634ddf36e26028a8806ba2d128ade2099eba108
akmhmgc <akimuhamaguchi@icloud.com>
2024-06-06 08:39:57 +0900

    143

---
+++ 143.reorder-list.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode id=143 lang=python3
+#
+# [143] Reorder List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        # 後半をreverseする
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # reverse second half
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+        
+        # merge
+        first, second = head, prev
+        while second:
+            tmp1 = first.next
+            tmp2 = second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
+
+# @lc code=end
+
