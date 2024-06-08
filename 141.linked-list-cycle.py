8972c968bed0a00b810a5a6563d247807908b225
akmhmgc <akimuhamaguchi@icloud.com>
2024-06-08 10:40:27 +0900

    141

---
+++ 141.linked-list-cycle.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=141 lang=python3
+#
+# [141] Linked List Cycle
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        for i in range(10**4 + 1):
+            if not head: return False
+            head = head.next
+        return True# @lc code=end
+
