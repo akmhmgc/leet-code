331b403f23560fb29b8a0c01c5d5cb6e5acf0741
akmhmgc <akimuhamaguchi@icloud.com>
2024-06-26 09:23:33 +0900

    230-2

---
+++ 230.kth-smallest-element-in-a-bst_recursive.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=230 lang=python3
+#
+# [230] Kth Smallest Element in a BST
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        inOrder = []
+
+        def doInOrderDFS(node):
+            if not node:
+                return
+            
+            doInOrderDFS(node.left)
+            inOrder.append(node.val)
+            doInOrderDFS(node.right)
+
+        doInOrderDFS(root)
+        return inOrder[k - 1]
+# @lc code=end
+
