82a2ed5cf006e880370c67e2b7d5df0e0afe6a68
akmhmgc <akimuhamaguchi@icloud.com>
2024-07-02 08:00:18 +0900

    70

---
+++ 70.climbing-stairs.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=70 lang=python3
+#
+# [70] Climbing Stairs
+#
+
+# @lc code=start
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [1] * (n + 1)
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[-1]
+# @lc code=end
+
